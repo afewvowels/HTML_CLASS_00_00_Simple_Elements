64682dd7fb646c16acb18bc5c7faf314b1efb926
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-09-04 09:46:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 // Javascript file
 
+
 function myFunction( ){
-	document.getElementById("demo").innerHTML = "Script has run.";
+	var active=0;
+	switch(active) {
+		case 0:
+			active = "1";
+			document.getElementById("demo").innerHTML = "Script has run.";
+			break;
+		case 1:
+			active = "0";
+			document.getElementById("demo").innerHTML = "Script has run twice.";
+			break;
+		default:
+			document.getElementById("demo").innerHTML = "Script error!";
+			break;
+	}
+}
+
+function refreshPage( ){
+	location.reload(true);
 }
